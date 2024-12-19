1715ba289eccc5365e3b3ad004cea35cdd3c32da
Matthew Gomez Cullen <mattgc1282@hotmail.co.uk>
2024-12-19 17:03:17 +0000

    Fix compilation error

--- sources/triggers.c
+++ sources/triggers.c
@@ -4,7 +4,7 @@
 
 #include "triggers.h"
 
-inline double getTimeDelta(struct timeval *current, struct timeval *prev) {
+static inline double getTimeDelta(struct timeval *current, struct timeval *prev) {
   return (current->tv_sec + current->tv_usec / 1000000.0) -
          (prev->tv_sec + prev->tv_usec / 1000000.0);
 }
